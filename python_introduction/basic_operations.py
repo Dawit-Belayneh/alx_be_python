7a1cef3c6c3a0e9498c00068736758974c5d6872
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-07-09 09:24:41 -0900

    arithmetic operation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,15 +1,13 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> # Define the two numbers
-... number1 = 10
-... number2 = 5
-... 
-... # Perform arithmetic operations
-... addition = number1 + number2
-... subtraction = number1 - number2
-... multiplication = number1 * number2
-... 
-... # Print results
-... print(f"Addition of {number1} and {number2} is {addition}")
-... print(f"Subtraction of {number1} and {number2} is {subtraction}")
-... print(f"Multiplication of {number1} and {number2} is {multiplication}")
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+
+difference = number1 - number2
+
+product = number1 * number2
+
+
+print('Addition of ', number1, ' and ', number2, ' is ', sum)
+print('Subtraction of ', number1, ' and ', number2, ' is ', difference)
+print('Multiplication of ', number1, ' and ', number2, ' is ', product)
